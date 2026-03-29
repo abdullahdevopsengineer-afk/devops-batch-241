a632b1e5d00d64b846e992d68837258e99344e41
abdullahasad45 <abdullah.asad@vaival.tech>
2026-03-29 21:39:33 +0500

    add commnet

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ res.json({message:"hello world"})
 })
 //this is commnet form dev branch
 //you have to check this code
+// now push this to github
 app.use(("/test",(req,res)=>{
 res.json({message:"This is a test"})
 
